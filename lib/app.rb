f60e58adbea5225deaf61a805d78485842c21335
Arran Oakes <arran.oakes@gmail.com>
2021-06-15 15:58:29 +0100

    prettied up the index

--- lib/app.rb
+++ lib/app.rb
@@ -3,14 +3,22 @@ require 'sinatra/reloader' if development?
 
 class Battle < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
   post '/game' do
-    @player_1 = params[:player_1]
-    @player_2 = params[:player_2]
-    erb :game
+    session[:player_1] = params[:player_1]
+    session[:player_2] = params[:player_2]
+    redirect('/play')
+  end
+
+  get '/play' do
+      @player_1 = session[:player_1]
+      @player_2 = session[:player_2]
+      erb :game
   end
 
   run! if app_file == $0
